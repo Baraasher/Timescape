85018a8d6f0eb94e1fb16b3956732f4c79060644
Baraasher <Baraa.programmer@gmail.com>
2023-04-07 21:54:24 +0300

    update layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
+import 'animate.css'
 import { FaTrash } from 'react-icons/fa';
 import { FaEdit } from 'react-icons/fa';
+
+
 
 function App() {
   const [selectedDate, setSelectedDate] = useState("");
@@ -25,10 +28,27 @@ function App() {
     setSelectedDate("");
     setNote("");
   };
-  const handleClearActions = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClearAll = () => {
+    setShowModal(true);
+  };
+
+  const handleClearAllConfirmed = () => {
     setSavedActions([]);
     localStorage.removeItem("savedActions");
+    setShowModal(false);
   };
+  
+  const handleClearActions = () => {
+    setShowModal(true);
+  };
+  
+  
+<button onClick={handleClearAll}>Clear All</button>
+
+  
 
   const handleDeleteAction = (index) => {
     const newSavedActions = [...savedActions];
@@ -46,15 +66,41 @@ function App() {
     setSavedActions(newSavedActions);
     localStorage.setItem("savedActions", JSON.stringify(newSavedActions));
   };
-  
   React.useEffect(() => {
+
+    var preloader = document.getElementsByClassName('preloader');
+  
+    window.addEventListener('load', function() {
+      preloader[0].style.display = "none";
+      document.body.classList.remove('loading');
+    });
+    
     const savedActionsJson = localStorage.getItem("savedActions");
     if (savedActionsJson) {
       setSavedActions(JSON.parse(savedActionsJson));
     }
   }, []);
+  
+  
   return (
+
+
+    <div class="loading">
+    <div class="preloader">
+      <div class="preloader-spinner"></div>
+    </div>
     <main className="App">
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Are you sure you want to clear all actions?</h2>
+            <div className="modal-buttons">
+              <button onClick={handleClearAllConfirmed}>Yes</button>
+              <button onClick={() => setShowModal(false)}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
       <aside className="AddDateSection">
         <h1>Timescape</h1>
         <label>
@@ -87,33 +133,38 @@ function App() {
             <div></div>
           </div>
         </label>
-<div class="buttons">
-        <button onClick={handleSaveAction}>Save</button>
-        <button onClick={handleClearActions}>Clear All</button>
+        <div class="buttons">
+          <button onClick={handleSaveAction}>Save</button>
+          <button onClick={handleClearAll}>Clear All</button>
         </div>
       </aside>
       <aside className="ShowDateSection">
-  <h2>Saved Actions:</h2>
-  {savedActions.length === 0 && <p>No actions saved yet.</p>}
-  {savedActions.map((action, index) => (
-    <div className="ActionCard" key={index}>
-  <div><h5>Date and Time:</h5><p> {action.date}</p></div>    
-   <div><h5>Note: </h5></div>   <p>{action.note}</p>
-      <div>
-
-<div className="ActionButtons">
-
-        <button onClick={() => handleEditAction(index)}><FaEdit size={14} color="#fff" /></button>
-        <button onClick={() => handleDeleteAction(index)}><FaTrash size={14} color="#fff" />
-</button>
-</div>
-
-      </div>
-    </div>
-  ))}
-</aside>
-
+        <h2>Saved Actions:</h2>
+        {savedActions.length === 0 && <p>No actions saved yet.</p>}
+        {savedActions.map((action, index) => (
+          <div className="ActionCard" key={index}>
+            <div>
+              <h5>Date and Time:</h5>
+              <p id="dateActionDiv"> {action.date}</p>
+            </div>    
+            <div>
+              <h5>Note: </h5>
+            </div>
+            <p>{action.note}</p>
+            <div className="ActionButtons">
+              <button onClick={() => handleEditAction(index)}>
+                <FaEdit size={14} color="#fff" />
+              </button>
+              <button onClick={() => handleDeleteAction(index)}>
+                <FaTrash size={14} color="#fff" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </aside>
     </main>
+  </div>
+  
   );
 }
 export default App;
